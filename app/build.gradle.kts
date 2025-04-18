23363b0c0f056ac549d87593c55f58f27747d4e8
emelypickler <emelypickler04@gmail.com>
2025-04-18 16:42:53 -0300

    atualização

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,9 @@ android {
 }
 
 dependencies {
+    implementation("androidx.activity:activity-compose:1.8.1")
+    implementation("androidx.compose.material3:material3:1.1.2")
+
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
